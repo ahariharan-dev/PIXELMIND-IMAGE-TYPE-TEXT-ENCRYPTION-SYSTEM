e9f976d4c1b627032197b48885f58125cf092878
hariharan77 <hariharan2842005@gmail.com>
2026-05-30 15:59:19 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -374,6 +374,6 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    app = create_app()
+    flask_app = create_app()
     port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port, debug=False)
+    flask_app.run(host="0.0.0.0", port=port, debug=False)
